16560f17eadd02c35235faf13ed9b582e910b4f6
Eshaan <eshaan.263@gmail.com>
2020-11-10 18:36:52 +0530

    changed the stats page with mdbReact which added smart feature

--- src/DisplayAll.jsx
+++ src/DisplayAll.jsx
@@ -1,40 +1,109 @@
 import React from 'react'
+import { MDBDataTable } from 'mdbreact';
 
-export default function DisplayAll({Placement,batch}) {
+export default function DisplayAll({ Placement, batch }) {
+    var data = {
+        columns: [
+                {
+                label: 'Name',
+                field: 'name',
+                sort: 'asc',
+                width: 150
+                },
+                {
+                label: 'Branch',
+                field: 'branch',
+                sort: 'asc',
+                width: 270
+                },
+                {
+                label: 'Package',
+                field: 'package',
+                sort: 'asc',
+                width: 200
+                },
+                {
+                    label: 'Company',
+                    field: 'company',
+                    sort: 'asc',
+                    width: 100
+                }
+        ],
+        rows:[]
+    }
+
+    function init()
+    {
+        var temp = [];
+        Placement.forEach((item) => {
+            const name = item.students[0].student_name;
+            const branch = item.students[0].department;
+            const company = item.company_name;
+            var Package = batch == 2022 ? item.intern_stipend : item.package;
+            if (Package === undefined)
+            {
+                return null;
+            }
+            else
+            {
+                var T =
+                {
+                    name: name,
+                    branch: branch,
+                    package: Package,
+                    company:company
+                }
+                temp.push(T);
+            }
+        })
+        data.rows = temp;
+        return null;
+    }
+
+    init(); 
+    console.log(data);
     return (
         <div>
-            <table class="table  table-striped">
-            <thead class="thead-dark">
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Branch</th>
-                <th scope="col">Package ({ batch==2021?"Lacs":"Thousand" })</th>
-                <th scope="col">Company</th>        
-                </tr>
-                {
-                    Placement.map((item) => {
-                        if (item.passout_batch == batch) {
-                            const name = item.students[0].student_name;
-                            const branch = item.students[0].department;
-                            const company = item.company_name;
-                            var Package =batch==2022?item.intern_stipend:item.package;
-                            return (
-                                <>
-                                <tr>
-                                    <td scope="row">#</td>
-                                    <td>{name}</td>
-                                    <td>{branch}</td>
-                                    <td>{Package}</td>
-                                    <td>{company}</td>
-                                </tr>
-                                </>
-                              )
-                        } 
-                    })       
-                }    
-            </thead>
-            </table>
+            <MDBDataTable
+            striped
+            bordered
+            small
+            data={data}
+            />
         </div>
     )
 }
+
+
+// <table class="table  table-striped">
+//             <thead class="thead-dark">
+//                 <tr>
+//                 <th scope="col">#</th>
+//                 <th scope="col">Name</th>
+//                 <th scope="col">Branch</th>
+//                 <th scope="col">Package ({ batch==2021?"Lacs":"Thousand" })</th>
+//                 <th scope="col">Company</th>        
+//                 </tr>
+//                 {
+//                     Placement.map((item) => {
+//                         if (item.passout_batch == batch) {
+//                             const name = item.students[0].student_name;
+//                             const branch = item.students[0].department;
+//                             const company = item.company_name;
+//                             var Package =batch==2022?item.intern_stipend:item.package;
+//                             return (
+//                                 <>
+//                                 <tr>
+//                                     <td scope="row">#</td>
+//                                     <td>{name}</td>
+//                                     <td>{branch}</td>
+//                                     <td>{Package}</td>
+//                                     <td>{company}</td>
+//                                 </tr>
+//                                 </>
+//                               )
+//                         } 
+//                     })       
+//                 }    
+//             </thead>
+//             </table>
