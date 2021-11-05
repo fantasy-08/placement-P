1ce79142ba53c9e6785bfa0dea22dc6fe70d08f9
Eshaan Gupta <59399447+fantasy-08@users.noreply.github.com>
2021-11-05 17:58:07 +0000

    Updates

---
+++ precache-manifest.f8e3744198dfd5479067f65d3793ab38.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3aa4a4b08a8a1a7910961d01e9fde8d",
+    "url": "/placement-P/index.html"
+  },
+  {
+    "revision": "e15a79c9aa9eacd71102",
+    "url": "/placement-P/static/css/2.943491bf.chunk.css"
+  },
+  {
+    "revision": "067924bfb330e682c2d1",
+    "url": "/placement-P/static/css/main.e3b8f9aa.chunk.css"
+  },
+  {
+    "revision": "e15a79c9aa9eacd71102",
+    "url": "/placement-P/static/js/2.5dbeb750.chunk.js"
+  },
+  {
+    "revision": "6240a96e598362eb23ddc4420154ddfd",
+    "url": "/placement-P/static/js/2.5dbeb750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072f0bce60f24e3462c2",
+    "url": "/placement-P/static/js/3.f648bae5.chunk.js"
+  },
+  {
+    "revision": "067924bfb330e682c2d1",
+    "url": "/placement-P/static/js/main.7692b271.chunk.js"
+  },
+  {
+    "revision": "d9141e780e05c2813108",
+    "url": "/placement-P/static/js/runtime-main.a926f975.js"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/placement-P/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/placement-P/static/media/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/placement-P/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/placement-P/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/placement-P/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/placement-P/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/placement-P/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/placement-P/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/placement-P/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/placement-P/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/placement-P/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/placement-P/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/placement-P/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/placement-P/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/placement-P/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/placement-P/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/placement-P/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "cec65ae4891a04d4af119faec51d1253",
+    "url": "/placement-P/static/media/heart.cec65ae4.svg"
+  }
+]);
