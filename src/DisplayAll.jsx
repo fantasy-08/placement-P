d714a3651063497f63521624daf3fb8f13f0ec90
Eshaan Gupta <59399447+fantasy-08@users.noreply.github.com>
2021-11-14 18:47:40 +0000

    added batch

--- src/DisplayAll.jsx
+++ src/DisplayAll.jsx
@@ -27,6 +27,12 @@ export default function DisplayAll({ Placement, batch }) {
                     field: 'company',
                     sort: 'asc',
                     width: 100
+                },
+                {
+                    label: 'Batch',
+                    field: 'batch',
+                    sort: 'asc',
+                    width: 50
                 }
         ],
         rows:[]
@@ -39,6 +45,7 @@ export default function DisplayAll({ Placement, batch }) {
             const name = item.students[0].student_name;
             const branch = item.students[0].department;
             const company = item.company_name;
+            const batch = item.passout_batch;
             var Package = batch === "2022" ? item.intern_stipend : item.package;
             if (Package === undefined || isNaN(Package) || Package == 0)
             {
@@ -51,7 +58,8 @@ export default function DisplayAll({ Placement, batch }) {
                     name: name,
                     branch: branch,
                     package: Package,
-                    company:company
+                    company: company,
+                    batch: 
                 }
                 temp.push(T);
             }
